c28e07b1e942f2c12f18661b03ab328dc440edd3
kim.shen <sjw_job@126.com>
2015-08-21 00:15:23 +0800

    fix maven plugin bug

--- kepler-maven/src/main/resources/archetype-resources/modules/service/src/main/java/com/kepler/impl/TestAppImpl.java
+++ kepler-maven/src/main/resources/archetype-resources/modules/service/src/main/java/com/kepler/impl/TestAppImpl.java
@@ -3,7 +3,7 @@ package com.kepler.impl;
 import com.kepler.TestApp;
 import com.kepler.annotation.Service;
 
-@Service(version = "0.0.1-test", autowired = true)
+@Service(version = "0.0.1-${artifactId}", autowired = true)
 public class TestAppImpl implements TestApp {
 
 	public String test(String kepler) throws Exception {
